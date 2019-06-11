1a4c6e48bce120e66f1803ac88a467576dc70b0b
cgalvar <julio_aguila@hotmail.es>
2019-06-10 19:16:33 -0500

    faltaba platform.modulename

--- src/index.ts
+++ src/index.ts
@@ -5,9 +5,9 @@ import {PLATFORM} from 'aurelia-pal';
 export function configure(config: FrameworkConfiguration) {
   // config.aurelia.use.plugin('aurelia-selectable');
   
-  config.plugin('@cgalvar/au-selectable')
-  .plugin('@cgalvar/au-some-value-converters')
-  .plugin('@cgalvar/au-toggle-button')
+  config.plugin(PLATFORM.moduleName('@cgalvar/au-selectable'))
+  .plugin(PLATFORM.moduleName('@cgalvar/au-some-value-converters'))
+  .plugin(PLATFORM.moduleName('@cgalvar/au-toggle-button'));
   
   config.globalResources([
     PLATFORM.moduleName('./elements/data-grid-view')
